f074f98314acce0843a0877bae7d2ead67d44130
Martin Lavie <martinlavie@Martins-MacBook-Pro.local>
2019-11-04 00:07:16 +0100

    [UPDATE] Ok I've learn some new things

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,33 @@ import GuessCount from './GuessCount/GuessCount';
 import Card from './Card/Card';
 
 export default class App extends React.Component {
+
+  user = {
+    admin: true
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
   render() {
+    const won = new Date().getSeconds() % 2 === 0
     return (
       <div className="App">
         <Header> </Header>
         <Separator></Separator>
         <GuessCount guesses={0} />
+        <p>{this.user.admin && <a href="/admin">Faire des trucs de VIP</a>}</p>
         <Separator></Separator>
         <div className="cardsContainer">
-          <Card card="😀" feedback="hidden" />
-          <Card card="🎉" feedback="justMatched" />
-          <Card card="💖" feedback="justMismatched" />
-          <Card card="🎩" feedback="visible" />
-          <Card card="🐶" feedback="hidden" />
-          <Card card="🐱" feedback="justMatched" />
+          <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+          <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+          <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+          <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
         </div>
+        {won && <p>GAGNÉ !</p>}
       </div>
     );
   };
--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -3,12 +3,11 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
+const Card = ({ card, feedback, onClick }) => {
+    return <div className={`card ${feedback}`} onClick={() => onClick(card)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>
     </div>
-)
-
+}
 export default Card
